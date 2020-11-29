822c53e741e1a01ccd16ace35ce667e244e254ec
ava brenneke <avabrenneke@gmail.com>
2020-11-28 22:56:29 -0800

    added uv function

--- weather.js
+++ weather.js
@@ -15,3 +15,34 @@ let searchHistory = [];
 function storeCities(){
     localStorage.setItem("city", JSON.stringify(cities)); 
 }
+//Current Weather Function
+function currentWeather(city, apiKey) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+    // UV index call
+    function currentUV(lat, lon, apiKey) {
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function (response) {
+            //Attach UV parameters from Google
+            cityUV.text("UV Index: " + response.value);
+            if (response.value < 3) {
+                cityUV.text("UV Index: " + response.value).attr("style", "background-color: green").append(" low");
+            }
+            else if (response.value >= 3 || response.value < 6) {
+                cityUV.text("UV Index: " + response.value).attr("style", "background-color: yellow").append(" moderate");
+            }
+            else if (response.value >= 6 || response.value < 8) {
+                cityUV.text("UV Index: " + response.value).attr("style", "background-color: orange").append(" high");
+            }
+            else if (response.value >= 8 || response.value < 11) {
+                cityUV.text("UV Index: " + response.value).attr("style", "background-color: red").append(" very high");
+            }
+            else {
+                cityUV.text("UV Index: " + response.value).attr("style", "background-color: violet").append(" extreme");
+            }
+        });
+    };
